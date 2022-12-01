45ff458688572cb86e1a0284511aff48a3725d37
DLloyd <dlloyd1992@gmail.com>
2022-12-01 14:35:46 -0800

    update

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,22 +1,69 @@
 -- Your code here
 -- 1
-INSERT INTO customers (name, phone)
-Values
-('Rachel', '111-111-1111');
+-- INSERT INTO customers (name, phone)
+-- Values
+-- ('Rachel', '111-111-1111');
 
--- 2
-INSERT INTO coffee_orders (is_redeemed)
-Values (0);
+-- -- 2
+-- INSERT INTO coffee_orders (is_redeemed)
+-- Values (0);
 
-Update customers
-set points = points +1
-where id = 1;
+-- Update customers
+-- set points = points +1
+-- where id = 1;
 
 
--- 3
-INSERT INTO customers (name, email, phone)
-Values
-('Monica', 'monica@friends.show', '222-222-2222'),
-('Phoebe', 'phoebe@friends.show', '333-333-3333');
-select * from customers;
-select * from coffee_orders;
+-- -- 3
+-- INSERT INTO customers (name, email, phone)
+-- Values
+-- ('Monica', 'monica@friends.show', '222-222-2222'),
+-- ('Phoebe', 'phoebe@friends.show', '333-333-3333');
+
+-- 4
+-- INSERT INTO coffee_orders (is_redeemed)
+-- Values (0), (0), (0);
+
+-- Update customers
+-- set points = points + 3
+-- where name = "Phoebe";
+
+-- 5
+-- INSERT INTO coffee_orders (is_redeemed)
+-- Values (0), (0), (0), (0), (0), (0), (0), (0);
+
+-- Update customers
+-- set points = points + 4
+-- where name = "Rachel";
+
+-- Update customers
+-- set points = points + 4
+-- where name = "Monica";
+
+-- 6
+-- select points from customers
+-- where name = "Monica";
+
+-- 7
+-- select points from customers
+-- where name = "Rachel";
+
+-- INSERT INTO coffee_orders (is_redeemed)
+-- Values (1);
+
+-- Update customers
+-- set points = points - 10
+-- where name = "Rachel";
+
+-- select points from customers
+-- where name = "Rachel";
+
+--8
+-- INSERT INTO customers (name, email)
+-- Values
+-- ("Joey", "joey@friends.show"),
+-- ("Chandler", "chandler@friends.show"),
+-- ("Ross", "ross@friends.show");
+
+-- 9
+-- select * from customers;
+-- select * from coffee_orders;
