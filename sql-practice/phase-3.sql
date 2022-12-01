574a9d2168bede2c3d7a3170570bc82943d6f73f
Fahd Ahsan <sfahsan@umich.edu>
2022-12-01 17:10:43 -0500

    part of 3

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,22 @@
 -- Your code here
+-- 1
+INSERT INTO customers (name, phone)
+Values
+('Rachel', '111-111-1111');
+
+-- 2
+INSERT INTO coffee_orders (is_redeemed)
+Values (0);
+
+Update customers
+set points = points +1
+where id = 1;
+
+
+-- 3
+INSERT INTO customers (name, email, phone)
+Values
+('Monica', 'monica@friends.show', '222-222-2222'),
+('Phoebe', 'phoebe@friends.show', '333-333-3333');
+select * from customers;
+select * from coffee_orders;
