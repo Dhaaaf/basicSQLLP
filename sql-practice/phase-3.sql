0431de8e5cb85c1cf666fd20679744a888c1910b
Fahd Ahsan <sfahsan@umich.edu>
2022-12-01 18:13:45 -0500

    Almost there

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -65,5 +65,65 @@
 -- ("Ross", "ross@friends.show");
 
 -- 9
+-- INSERT INTO coffee_orders (is_redeemed)
+-- Values (0), (0), (0), (0), (0), (0);
+
+-- Update customers
+-- set points = points + 6
+-- where name = "Ross";
+
+-- 10
+-- INSERT INTO coffee_orders (is_redeemed)
+-- Values (0), (0), (0);
+
+-- Update customers
+-- set points = points + 3
+-- where name = "Monica";
+
+-- 11
+-- Select points from customers
+-- where name = "Phoebe";
+
+-- Insert INTO coffee_orders (is_redeemed)
+-- Values (0);
+
+-- Update customers
+-- set points = points + 1
+-- where name = "Phoebe";
+
+-- 12
+-- Delete from coffee_orders
+-- where id = "19";
+
+-- Delete from coffee_orders
+-- where id ="18";
+
+-- Update customers
+-- set points = points -2
+-- where name = "Ross";
+
+-- 13
+-- Insert INTO coffee_orders (is_redeemed)
+-- Values (0), (0);
+
+-- Update customers
+-- set points = points +2
+-- where name = "Joey";
+
+-- 14
+-- select points from customers
+-- where name = "Monica";
+
+-- Insert INTO coffee_orders (is_redeemed)
+-- Values (1);
+
+-- Update customers
+-- set points = points -10
+-- where name = "Monica";
+
+-- 15
+-- Delete from customers
+-- where name = "Chandler";
+
 -- select * from customers;
 -- select * from coffee_orders;
